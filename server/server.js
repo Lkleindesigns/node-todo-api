40742fd37deee0913c0b63e37d3c2b2cc1da2fb7
Logan Klein <lkleindesigns@gmail.com>
2017-07-22 20:13:59 -0500

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -20,13 +20,14 @@ app.post('/todos', (req, res) => {
     res.status(400).send(e)
   })
 })
-// Create
 
-// Read
-
-// Update
-
-// Destroy
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos})
+  }, (e) => {
+    res.status(400).send(e)
+  })
+})
 
 app.listen(3000, () => {
   console.log('Started on port 3000!')
