0e6c826062a1ecef2328890baf17803676f2ab04
Logan Klein <lkleindesigns@gmail.com>
2017-07-23 23:31:40 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+const {ObjectID} = require('mongodb')
 
 var {mongoose} = require('./db/mongoose.js')
 var {Todo} = require('./models/todo.js')
@@ -27,6 +28,33 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e)
   })
+})
+
+// GET /todos/:id
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id
+
+  // Valid id using isValid
+  if (!ObjectID.isValid(id)) {
+    // 404 -- send back empty body -send
+    return res.status(404).send()
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send()
+    }
+
+    res.send({todo})
+  }).catch((e) => {
+    res.status(400).send()
+  })
+  //findById
+    // success
+      // if todo - send it back
+      // if no todo - send back 404 with empty body
+    // error
+      // 400 - send empty body back
 })
 
 app.listen(3000, () => {
