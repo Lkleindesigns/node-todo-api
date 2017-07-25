864568dbe0c9e217553043a3969da0241fe2c2c3
Logan Klein <lkleindesigns@gmail.com>
2017-07-24 23:04:18 -0500

    fix mongodb connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
+mongoose.connect('mongodb://lklein:test1@ds043981.mlab.com:43981/node-todo-api' || 'mongodb://localhost:27017/TodoApp')
 
 module.exports = {mongoose}
