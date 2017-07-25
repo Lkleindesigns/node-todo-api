2a4924bc4ad784496b194a680e13ea736a9c25ea
Logan Klein <lkleindesigns@gmail.com>
2017-07-25 12:08:10 -0500

    add environment mongoDB URI variable

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://lklein:test123@ds043981.mlab.com:43981/node-todo-api'
+  mlab: process.env.MONGODB_URI
 }
 mongoose.connect(db.mlab || db.localhost)
 
