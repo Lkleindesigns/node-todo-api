9b2a1c714733d98aedb10eae742ce8924eccdec1
Logan Klein <lkleindesigns@gmail.com>
2017-07-24 23:04:49 -0500

    fix password

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://lklein:test1@ds043981.mlab.com:43981/node-todo-api' || 'mongodb://localhost:27017/TodoApp')
+mongoose.connect('mongodb://lklein:test123@ds043981.mlab.com:43981/node-todo-api' || 'mongodb://localhost:27017/TodoApp')
 
 module.exports = {mongoose}
