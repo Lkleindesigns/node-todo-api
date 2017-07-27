56e0f66a98cefc7a44be9b1ca5909b918cfb07f8
Logan Klein <lkleindesigns@gmail.com>
2017-07-27 10:15:42 -0500

    Setup seperate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,6 @@
 var mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: process.env.MONGODB_URI
-}
-mongoose.connect(db.mlab || db.localhost)
+mongoose.connect(process.env.MONGODB_URI)
 
 module.exports = {mongoose}
